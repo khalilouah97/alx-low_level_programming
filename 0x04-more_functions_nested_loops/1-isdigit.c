06a686c47c0345a95bb28b88d868e34db4eab791
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-10-12 17:00:33 -0700

    d

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * _isdigit - a function that checks for a digit (0 through 9).
+ *
+ * @c: the character to be checked.
+ *
+ * Return: 1 if it is a digit,
+ *         0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,4 +17,4 @@ void print_triangle(int size);
 void print_number(int n);
 
 
-#endif /* MAIN_H *//
+#endif /* MAIN_H */
