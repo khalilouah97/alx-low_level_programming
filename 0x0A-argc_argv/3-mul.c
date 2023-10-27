aaf8eb9649bfa7c942fc86d39f6694cfa6927267
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-10-27 13:10:42 -0700

    f

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print the result of the multiplication
+ *
+ * @argc: variable
+ * @argv: variable
+ *
+ * by: laila tabourit
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
