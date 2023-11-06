4bbfb1f3b563c1d05e9be949a4753155a2790cc5
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-06 13:05:57 -0800

    f

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,21 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * free_dog - free memory
- * @d: struct dog
- *
- * by : lailitaa
+ * free_dog - a function that frees dogs.
+ * @d: parameter.
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->name);
-		free(d->owner);
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
 		free(d);
 	}
 }
