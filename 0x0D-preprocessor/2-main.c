04dc30ea6f329860f15849a275b21f59961f40c8
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-06 11:25:19 -0800

    f

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the name of the file it was compiled from.
- *   *
- *    * by: AJJI OUSSAMA.
- *     * Return: always 0 (success).
- *      */
+ * main - prints the name of the file it was compiled from.
+ *
+ * by: AJJI OUSSAMA.
+ * Return: always 0 (success).
+ */
 int main(void)
 {
 		printf("%s\n", __FILE__);
