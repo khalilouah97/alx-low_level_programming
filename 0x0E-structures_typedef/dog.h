15b9a04007184badf1af7604fe12a504691c4587
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-06 11:37:44 -0800

    f

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct of dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
