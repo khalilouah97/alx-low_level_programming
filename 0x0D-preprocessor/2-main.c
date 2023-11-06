fe6b75c53e3fddb677cb59cc77e53d3e50e17fd2
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-06 11:23:40 -0800

    f

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  * main - prints the name of the file it was compiled from.
+ *   *
+ *    * by: AJJI OUSSAMA.
+ *     * Return: always 0 (success).
+ *      */
+int main(void)
+{
+		printf("%s\n", __FILE__);
+			return (0);
+}
