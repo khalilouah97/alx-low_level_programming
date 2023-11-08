7add8176799e71d6667e1aa86843a6d30208626e
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-08 11:32:22 -0800

    f

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function given as a parameter on each element
+ * @array: array
+ * @size: size of array
+ * @action: pointera
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
