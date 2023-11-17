643be511bc27508af7c24f4f0ee50d405820a2dc
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-16 21:46:27 -0800

    k

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first - function print
+ *
+ * Return: void
+ */
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
