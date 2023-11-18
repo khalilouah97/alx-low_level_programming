2c5525c1173241d15621c4368e66e7395b33284c
khalil ouahmid <khalil.ouahmid@gmail.com>
2023-11-17 16:03:00 -0800

    d

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * *add_node - adds new node
+ * @head: argument
+ * @str: argument
+ * Return: list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *lst;
+
+	lst = malloc(sizeof(list_t));
+	if (!lst)
+		return (NULL);
+	lst->str = strdup(str);
+	lst->len = strlen(str);
+	lst->next = *head;
+	*head = lst;
+	return (lst);
+}
